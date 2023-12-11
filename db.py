469b4b9b78b79baad4d22e0c6fdde4a598194ae5
gordeyzav <gordey0511@yandex.ru>
2023-12-12 00:00:29 +0300

    first commit

---
+++ db.py
@@ -0,0 +1,139 @@
+import random
+import datetime
+from os import environ
+from sys import stderr
+
+import certifi
+import pymongo as pymongo
+from bson import ObjectId
+
+# TOKEN = environ["TOKEN"][:-1].strip()  # '5791033550:AAF_2TRXtRpZa1MqG7g-53pJTQItZVLbY3c'
+TOKEN = "6681679120:AAE9X_jHYVzv4t0n7MbZwVIt6qZE5Isp3Ps"
+print(TOKEN, file=stderr)
+client = pymongo.MongoClient(
+	"mongodb+srv://admin:X6TdCST1pqAEyboy@cluster0.fie8d2g.mongodb.net/?retryWrites=true&w=majority",
+	tlsCAFile=certifi.where())
+database = client["RandomCoffee"]
+users = database["users"]
+questions = database["questions"]
+tags = database["tags"]
+matches = database["matches"]
+common_information = database["common_information"]
+
+
+class Config:
+	def __init__(self):
+		self.constants = common_information.find_one({"bot_id": TOKEN})
+
+	def __getattr__(self, item):
+		return self.constants.get(item)
+
+
+cfg = Config()
+
+
+def get_rate_for_pair(user1, user2):
+	return random.randint(1, 100)
+
+
+def insert_user(user):
+	users.insert_one(user)
+
+
+def find_user(tid: int, bot_id: str):
+	user = users.find_one({"tid": tid, "bot_id": bot_id})
+	if user is None:
+		user = {
+			"tid": tid,
+			"bot_id": bot_id,
+			"state": cfg.START_QUESTION,
+			"last_question": cfg.START_QUESTION,
+			"data": {},
+			"waiting_match": False,
+		}
+		insert_user(user)
+	return user
+
+
+def update_user(user: dict, bot_id: str):
+	users.update_one({"tid": user["tid"], "bot_id": bot_id}, {"$set": user})
+
+
+def find_question(question_id: ObjectId):
+	return questions.find_one({"_id": question_id})
+
+
+def find_data_questions():
+	return sorted(filter(lambda x: x.get("title_edit"), questions.find({})), key=lambda x: x.get("title_edit"))
+
+
+def get_user_data(tid, bot_id):
+	def load_data(data):
+		if isinstance(data, str):
+			return data
+		return find_tag(data)["name"]
+
+	user = find_user(tid, bot_id)
+	text = '\n'.join(
+		f'<b>{x["title_edit"]}:</b> {load_data(user["data"][str(x["_id"])])}'
+		for x in find_data_questions()
+	)
+	return text
+
+
+def get_profile(tid: int, bot_id: str):
+	return f'<b>{cfg.TEXT_PROFILE}</b>\n{get_user_data(tid, bot_id)}'
+
+
+def get_user_name(tid: int, bot_id: str):
+	user_data = find_user(tid, bot_id).get('data', dict())
+	for key in user_data:
+		if ObjectId(key) == cfg.NAME_QUESTION:
+			return user_data[key]
+	return 'пользователь'
+
+
+def get_match_profile(tid: int, bot_id: str):
+	username = f'<a href="tg://user?id={tid}">{get_user_name(tid, bot_id)}</a>'
+	return f'{cfg.SCHEDULE_TEXTS[-1]} {username}\n{get_user_data(tid, bot_id)}'
+
+
+def find_tag(tag_id):
+	return tags.find_one(tag_id)
+
+
+def find_match(match_id):
+	return matches.find_one(match_id)
+
+
+def find_users_waiting_match():
+	users_waiting_match = users.find({"waiting_match": True})
+	return users_waiting_match
+
+
+def find_user_not_waiting_match():
+	user_not_waiting_match = users.find({"waiting_match": False})
+	return user_not_waiting_match
+
+
+def insert_match_in_user(user_id, bot_id, match_id):
+	users.update_one({"tid": user_id, "bot_id": bot_id}, {"$push": {"matches": match_id}}, upsert=True)
+
+
+def insert_match(user1_id, user2_id, bot_id):
+	match = matches.insert_one({
+		"users": [user1_id, user2_id],
+		"time": datetime.datetime.now(),
+		"delivered": False,
+	})
+	inserted_id = match.inserted_id
+	insert_match_in_user(user1_id, bot_id, inserted_id)
+	insert_match_in_user(user2_id, bot_id, inserted_id)
+
+
+def update_match(match):
+	matches.update_one({"_id": match["_id"]}, {"$set": match})
+
+
+def find_all_matches():
+	return matches.find({"delivered": False})
---
+++ main.py
@@ -0,0 +1,170 @@
+import os
+import logging
+import asyncio
+
+import aioschedule
+from aiogram import Dispatcher, Bot
+from aiogram.utils import executor
+from aiogram.types import (
+	Message, ContentType, PreCheckoutQuery, CallbackQuery, InlineKeyboardMarkup,
+	InlineKeyboardButton, ReplyKeyboardMarkup, KeyboardButton, LabeledPrice,
+)
+from freezegun import freeze_time
+
+import db
+from db import cfg
+
+
+logging.basicConfig(level=logging.INFO)
+bot = Bot(token=cfg.bot_id)
+dp = Dispatcher(bot)
+
+
+def zip_zip(buttons: list) -> InlineKeyboardMarkup:
+	markup = InlineKeyboardMarkup()
+	if max(map(lambda x: len(x.text), buttons)) > cfg.MAX_SYMBOLS_ROW:
+		for x in buttons:
+			markup.add(x)
+		return markup
+	for x, y in zip(buttons[::2], buttons[1::2]):
+		markup.row(x, y)
+	if len(buttons) % 1:
+		markup.add(buttons[-1])
+	return markup
+
+
+base_keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
+# base_keyboard.add(KeyboardButton(cfg.BUTTON_MATCH))
+base_keyboard.row(KeyboardButton(cfg.BUTTON_PROFILE), KeyboardButton(cfg.BUTTON_SOS))
+profile_keyboard = zip_zip(list(map(
+	lambda x: InlineKeyboardButton(text=x["title_edit"], callback_data=f'profile:{x["_id"]}'),
+	db.find_data_questions()
+)))
+
+
+async def trigger(question_id: db.ObjectId, user: dict):
+	if question_id is None:
+		await bot.send_message(
+			user["tid"],
+			db.get_profile(user["tid"], cfg.bot_id),
+			parse_mode='html',
+			reply_markup=base_keyboard
+		)
+		return
+	question = db.find_question(question_id)
+	if question.get("type_answer") is None:
+		user["state"] = question.get("next_question")
+		if user["last_question"] is not None:
+			user["last_question"] = user["state"]
+		await bot.send_message(user["tid"], question["text"])
+		db.update_user(user, cfg.bot_id)
+		await trigger(user["state"], user)
+	elif question["type_answer"] == "text":
+		await bot.send_message(user["tid"], question["text"])
+	elif question["type_answer"] == "single_button":
+		await bot.send_message(
+			user["tid"],
+			question["text"],
+			reply_markup=zip_zip(
+				list(map(
+						lambda x: InlineKeyboardButton(text=db.find_tag(x)["name"], callback_data=str(x)),
+						question["fields_answer"]
+					))
+			)
+		)
+	elif question["type_answer"] == "payment":
+		await bot.send_invoice(
+			user["tid"],
+			title=cfg.PAID_TITLE,
+			description=cfg.PAID_DESCRIPTION,
+			provider_token=cfg.PAY_TOKEN,
+			currency='rub',
+			prices=[LabeledPrice(label=cfg.PAID_TITLE, amount=cfg.VALUE_PRICE * 100)],
+			payload='test-invoice-payload',
+		)
+
+
+@dp.message_handler(commands=['start'])
+async def start(message: Message):
+	user = db.find_user(message.from_user.id, cfg.bot_id)
+	await trigger(user["state"], user)
+
+
+async def go_next(user_id: int, message: Message, data: str, clear_markup: bool = False):
+	user = db.find_user(user_id, cfg.bot_id)
+	question = db.find_question(user["state"])
+	if clear_markup:
+		data = db.ObjectId(data)
+		await message.edit_text(
+			text=f'{message.text}\n\n👉 <b>{db.find_tag(data)["name"]}</b>',
+			parse_mode='html',
+			reply_markup=None
+		)
+	user["data"][str(question["_id"])] = data
+	if user.get("edit_flag"):
+		user["state"] = None
+		user["edit_flag"] = False
+	else:
+		user["state"] = question.get("next_question")
+	if user["last_question"] is not None:
+		user["last_question"] = user["state"]
+	db.update_user(user, cfg.bot_id)
+	await trigger(user["state"], user)
+
+
+@dp.message_handler()
+async def message_trigger(message: Message):
+	user = db.find_user(message.from_user.id, cfg.bot_id)
+	if user["state"] is not None:
+		await go_next(message.from_user.id, message, message.text)
+	elif message.text == cfg.BUTTON_SOS:
+		await message.answer(cfg.TEXT_SOS)
+	elif message.text == cfg.BUTTON_PROFILE:
+		await message.answer(
+			db.get_profile(message.from_user.id, cfg.bot_id),
+			parse_mode='html',
+			reply_markup=profile_keyboard,
+		)
+
+
+
+@dp.callback_query_handler(lambda call: call.data.startswith('profile'))
+async def profile_edit_button(call: CallbackQuery):
+	question_id = db.ObjectId(call.data.split(':')[1])
+	user = db.find_user(call.from_user.id, cfg.bot_id)
+	user["edit_flag"] = True
+	user["state"] = question_id
+	db.update_user(user, cfg.bot_id)
+	await call.message.edit_reply_markup(None)
+	await trigger(question_id, user)
+
+
+@dp.callback_query_handler()
+async def button_trigger(call: CallbackQuery):
+	user = db.find_user(call.from_user.id, cfg.bot_id)
+	if user["state"] is not None:
+		await go_next(call.from_user.id, call.message, call.data, clear_markup=True)
+
+
+@dp.pre_checkout_query_handler(lambda query: True)
+async def pre_checkout_query(pre_checkout_q: PreCheckoutQuery):
+	await bot.answer_pre_checkout_query(pre_checkout_q.id, ok=True)
+
+
+@dp.message_handler(content_types=ContentType.SUCCESSFUL_PAYMENT)
+async def successful_payment(message: Message):
+	user = db.find_user(message.from_user.id, cfg.bot_id)
+	question = db.find_question(user["state"])
+	user["waiting_match"] = True
+	user["state"] = question.get("next_question")
+	db.update_user(user, cfg.bot_id)
+	await bot.send_message(user["tid"], cfg.TEXT_PAID)
+	await trigger(user["state"], user)
+
+
+if __name__ == '__main__':
+	try:
+		executor.start_polling(dp, skip_updates=True)
+	except Exception as exc:
+		logging.error(exc)
+		os._exit(0)
